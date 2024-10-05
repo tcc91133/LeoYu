3c04e6aec3ee56016efd55d0b73456aa9e0f5db7
mick60213 <mick60213@gmail.com>
2024-10-05 11:17:41 +0800

    Melee1st

--- Assets/Chen/Gun.cs
+++ Assets/Chen/Gun.cs
@@ -1,75 +1,45 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    protected Vector2 mousePos;       // Mouse position in world space
-    protected Camera mainCamera;      // Reference to the main camera
+    protected Vector2 mousePos;       // 鼠标位置
+    protected Camera mainCamera;      // 主摄像机引用
+    private Vector3 originalScale;    // 保存原始缩放比例
 
     protected virtual void Start()
     {
-        // Cache the main camera for efficiency
+        // 缓存主摄像机以提高效率
         mainCamera = Camera.main;
+        originalScale = transform.localScale; // 保存原始缩放比例
     }
 
     protected virtual void Update()
     {
-        // Get the mouse position in world space
+        // 获取鼠标在世界空间的位置
         mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
-        // Calculate the direction from gun to mouse
+        // 计算从枪到鼠标的方向
         Vector2 direction = mousePos - (Vector2)transform.position;
 
-        // Calculate the angle to rotate the gun
+        // 计算旋转角度
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        // Apply the rotation based on the angle
+        // 应用旋转
         transform.rotation = Quaternion.Euler(0, 0, angle);
 
-        // Flip the gun's scale horizontally if the mouse is on the left side
+        // 保持枪的缩放不变
+        transform.localScale = originalScale;
+
+        // 根据鼠标位置翻转枪的缩放
         if (mousePos.x < transform.position.x)
         {
-            // Flip the gun to face the left
-            transform.localScale = new Vector3(1, -1, 1); // Flip on Y-axis to mirror horizontally
+            transform.localScale = new Vector3(originalScale.x, -originalScale.y, originalScale.z); // 翻转
         }
         else
         {
-            // Face right (default direction)
-            transform.localScale = new Vector3(1, 1, 1); // Reset flip when on the right
+            transform.localScale = originalScale; // 重置翻转
         }
     }
 }
-
-
-
-
-
-
-/*if (Input.GetButton("Fire1"))
-{
-    if (timer == 0)
-    {
-        timer = interval;
-        //Fire();
-    }
-}*/
-
-
-/*protected virtual void Fire()
-{
-    animator.SetTrigger("Shoot");
-
-    GameObject bullet = Instantiate(bulletPrefab, muzzlePos.position, Quaternion.identity);
-    //GameObject bullet = ObjectPool.Instance.GetObject(bulletPrefab);
-    bullet.transform.position = muzzlePos.position;
-
-    float angel = Random.Range(-5f, 5f);
-    //bullet.GetComponent<Bullet>().SetSpeed(Quaternion.AngleAxis(angel, Vector3.forward) * direction);
-    bullet.GetComponent<Bullet>().SetSpeed(direction);
-    Instantiate(shellPrefab, shellPos.position, shellPos.rotation);
-    //GameObject shell = ObjectPool.Instance.GetObject(shellPrefab);
-    //shell.transform.position = shellPos.position;
-    //shell.transform.rotation = shellPos.rotation;
-}*/
-
--- Assets/Script/Attack.cs
+++ Assets/Script/Attack.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -7,16 +7,18 @@ using UnityEngine.Events;
 
 public class Attack : MonoBehaviour
 {
-    [SerializeField] private string targetTag;
-    [SerializeField] private UnityEvent attack;
+    [SerializeField] private string targetTag;            // 目标标签
+    [SerializeField] private UnityEvent attack;            // 攻击事件
+    [SerializeField] private float damageCooldown = 1f;    // 每个目标的伤害冷却时间
+    [SerializeField] private int damageAmount = 1;         // 攻击造成的伤害值，可以在 Inspector 中调整
 
-    private bool _canAttack = true;
+    private Dictionary<Collider2D, float> lastDamageTime = new Dictionary<Collider2D, float>(); // 记录每个目标的最后伤害时间
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         DealDamage(col);
     }
-     
+
     private void OnTriggerStay2D(Collider2D other)
     {
         DealDamage(other);
@@ -24,20 +26,14 @@ public class Attack : MonoBehaviour
 
     private void DealDamage(Collider2D other)
     {
-        if (!_canAttack) return;
+        if (!other.CompareTag(targetTag)) return;
 
-        if (other.CompareTag(targetTag))
+        if (!lastDamageTime.TryGetValue(other, out float lastAttackTime) || Time.time >= lastAttackTime + damageCooldown)
         {
             var damageable = other.GetComponent<Damageable>();
-            damageable.TakeDamage(1);
-            TimersManager.SetTimer(this, 1, CanAttack);
-            _canAttack = false;
+            damageable.TakeDamage(damageAmount); // 使用 damageAmount 造成伤害
+            lastDamageTime[other] = Time.time; // 更新目标的最后伤害时间
             attack.Invoke();
         }
-    }
-
-    private void CanAttack()
-    {
-        _canAttack = true;
     }
 }
