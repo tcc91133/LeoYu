1c7a1f8061ef9c890adef926660847ba627f69e1
cc506 <9b0k0901@stust.edu.tw>
2025-03-27 23:46:11 +0800

    0327

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -1,19 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class CameraController : MonoBehaviour
 {
     private Transform target;
-    // Start is called before the first frame update
+
+    public Vector2 minLimit; // 摄像机可移动的最小 X、Y 值
+    public Vector2 maxLimit; // 摄像机可移动的最大 X、Y 值
+
     void Start()
     {
         target = FindObjectOfType<PlayerController>().transform;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        if (target == null) return;
+
+        // 计算新位置
+        float clampedX = Mathf.Clamp(target.position.x, minLimit.x, maxLimit.x);
+        float clampedY = Mathf.Clamp(target.position.y, minLimit.y, maxLimit.y);
+
+        // 设定摄像机位置
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
